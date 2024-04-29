fe0667b10b2e223504fa88f2937b552b9c28ebcd
Dima-Ibragimov <168379280+Dima-Ibragimov@users.noreply.github.com>
2024-04-30 02:14:29 +0300

    Add files via upload

---
+++ module2.1.py
@@ -0,0 +1,34 @@
+print('пример 1')
+x=29
+print('Hello!')
+if x<0:
+    print('Ok')
+print('Bay')
+input()
+print('Пример 2')
+a,b=8,6
+if a>b:
+    print('a>b')
+if a>b and a>0:
+    print('Успех')
+if (a>b) and (a>0 or b<1000):
+    print('Успех')
+if 5<a and b<10:
+    print('Успех')
+    input()
+print("Пример 3")
+if '35' > '126':
+    print('Успех')
+if "123">'12':
+    print("Успех")
+if [1,2]>[1,1]:
+    print('Успех')
+input()
+print('Пример 4')
+if '6'<7:
+    print('Успех')
+if[5,6]>5:
+    print('Успех')
+if '6'!=5:
+    print('Успех')
+input()
